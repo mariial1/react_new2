6742ccdeb7f16f4394e08f87bad3bd624df093bb
Mariia Trusova <tmv_2008@ukr.net>
2021-08-16 00:45:05 +0300

    ++++

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Simpson from "./components/simpson/Simpson";
 export default function App() {
   return (
     <div className="simpsons">
-    <Simpson name={'Bart'} surname={'Simpson'} age={'10'}
+      <Simpson name={'Bart'} surname={'Simpson'} age={'10'}
              info={'Бартолою ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.'}
              photo=<img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png" alt=""/> />
+      <Simpson name={'Bart'} surname={'Simpson'} age={'10'}
+             info={'Бартолою ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.'}
+             photo=<img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png" alt=""/> />
+      <Simpson name={'Bart'} surname={'Simpson'} age={'10'}
+               info={'Бартолою ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.'}
+               photo=<img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png" alt=""/> />
+      <Simpson name={'Bart'} surname={'Simpson'} age={'10'}
+               info={'Бартолою ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.'}
+               photo=<img src="https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png" alt=""/> />
     </div>
   );
 }
